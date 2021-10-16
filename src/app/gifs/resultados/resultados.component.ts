2d5d7cb0a13538fd51b4ca09715481c5cb5b8276
Jakson Casas <jaksoncasas@gmail.com>
2021-10-16 18:07:52 -0500

    Agregar gifs servicio a resultado component

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
   styleUrls: ['./resultados.component.css']
 })
-export class ResultadosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ResultadosComponent{
+  
+  get resultados(){
+    return this.gifsService.resultado;
   }
+  constructor(private gifsService: GifsService) { }
+
 
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+    private apiKey:string = 'XCkB0kXpgmV52gzyviWMbWxKrEONcG0T';
+
     private _historial: string[] = [];
+    //TODO: Cambiar any por su tipo 
+    public resultado: any[] = [];
 
     get historial(){
       return [...this._historial];
     }
+
+    constructor(private http:HttpClient){}
+
     buscarGifs(query:string = ''){
       query = query.trim().toLowerCase(); //Convertir a minuscula
 
@@ -17,7 +25,12 @@ export class GifsService {
         this._historial.unshift(query); //unshift->La ultima aparece al principio
         this._historial = this._historial.splice(0,10); //Corta la cantidad de array[]
       }
-      
-      console.log(this._historial);
+      //Consumir API Gif
+      this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=XCkB0kXpgmV52gzyviWMbWxKrEONcG0T&q=dragon ball z&limit=10`)
+      .subscribe((resp:any) => {
+        console.log(resp.data);
+        this.resultado = resp.data;
+      });
+      //console.log(this._historial);
     }
 }
