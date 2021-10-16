3818c6a4f6726c2952e4507d5ec26d93decfc128
Jakson Casas <jaksoncasas@gmail.com>
2021-10-16 00:02:01 -0500

    update

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,11 +10,12 @@ export class GifsService {
       return [...this._historial];
     }
     buscarGifs(query:string = ''){
-      query = query.trim().toLowerCase();
+      query = query.trim().toLowerCase(); //Convertir a minuscula
 
+      //Si el query no existe lo incluye
       if(!this._historial.includes(query)){
         this._historial.unshift(query); //unshift->La ultima aparece al principio
-        this._historial = this._historial.splice(0,10);
+        this._historial = this._historial.splice(0,10); //Corta la cantidad de array[]
       }
       
       console.log(this._historial);
